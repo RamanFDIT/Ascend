f37db01c5a5e1fdb62c5722afc5f5eae0cff2479
Ramandeep Singh <rs9896158641@gmail.com>
2025-10-28 17:11:22 -0700

    React installed and working on Navigation

---
+++ Ascend/src/App.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./Components/Navigation/Navigation.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+    </BrowserRouter>  
+  );
+}
+export default App;
